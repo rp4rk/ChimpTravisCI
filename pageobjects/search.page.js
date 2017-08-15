89021d6ae1fb16857d8efb54a50e496bd8e4dea1
Ryan Park <ryan.park@unosquare.com>
2017-08-15 10:31:13 +0100

    Remove old travis CI badge

--- pageobjects/search.page.js
+++ pageobjects/search.page.js
@@ -3,25 +3,29 @@
 const page = require('./page');
 
 const searchPage = Object.create(page, {
-    /**
-     * define elements
-     */
+  /**
+   * define elements
+   */
   searchBtn: { get() { return browser.element('button[type="submit"]'); } },
   searchField: { get() { return browser.element('input[name="q"]'); } },
   searchResults: { get() { return browser.element('#search'); } },
 
-    /**
-     * define or overwrite page methods
-     */
-  open: { value() {
-    page.open.call(this, '');
-  } },
+  /**
+   * define or overwrite page methods
+   */
+  open: {
+    value() {
+      page.open.call(this, '');
+    }
+  },
 
-  searchFor: { value(value) {
-    this.searchField.setValue(value);
-    this.searchBtn.click();
-    this.searchResults.waitForVisible();
-  } },
+  searchFor: {
+    value(value) {
+      this.searchField.setValue(value);
+      this.searchBtn.click();
+      this.searchResults.waitForVisible();
+    }
+  },
 });
 
 module.exports = searchPage;
