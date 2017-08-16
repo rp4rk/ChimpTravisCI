d33163dd72d5df159c617dbc223883bc91641da0
Ryan Park <ryan.park@unosquare.com>
2017-08-16 15:34:29 +0100

    Move deploy into script

--- failure_deploy.sh
+++ failure_deploy.sh
@@ -11,12 +11,12 @@ SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-cd e2e_output
+cd e2e_output/report
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
 # Now let's go have some fun with the cloned repo
-cd out
+cd e2e_output/report
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
@@ -36,7 +36,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy.enc -out ../deploy -d
 chmod 600 ../deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
