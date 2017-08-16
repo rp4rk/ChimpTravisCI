a865f6b2a9ff87dda1b9a4dbbec66fe1925b9295
Ryan Park <ryan.park@unosquare.com>
2017-08-16 14:16:54 +0100

    add current dir

--- generate-report.js
+++ generate-report.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 
 
 const dirs = p => fs.readdirSync(p).filter(f => fs.statSync(p+"/"+f).isDirectory())
-console.log(dirs, ' - Directories');
+console.log(dirs(__dirname), ' - Directories');
 
 // Generate report
 reporter.generate(options);
