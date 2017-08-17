00a849467a244c5c55972d0ffe6f96dac06ae7cf
Ryan Park <ryan.park@unosquare.com>
2017-08-17 13:50:51 +0100

    Remove passkey

--- decrypt.sh
+++ decrypt.sh
@@ -7,7 +7,6 @@ ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ./id_rsa.enc -out ./id_rsa -d
-ls
 chmod 600 ./id_rsa
 eval `ssh-agent -s`
 ssh-add ./id_rsa
