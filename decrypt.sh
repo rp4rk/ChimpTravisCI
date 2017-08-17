ad67c42e21a488edaa51d215f906b7c6b9d9af16
Ryan Park <ryan.park@unosquare.com>
2017-08-17 11:22:40 +0100

    List files

--- decrypt.sh
+++ decrypt.sh
@@ -7,4 +7,5 @@ ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ./id_rsa.enc -out ./id_rsa -d
+ls
 chmod 600 ../deploy_key
