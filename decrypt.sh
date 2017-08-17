5b7bcaa13ea9f2a5fee15a077b40e06fa1920d95
Ryan Park <ryan.park@unosquare.com>
2017-08-17 10:32:08 +0100

    Move decryption into own script

---
+++ decrypt.sh
@@ -0,0 +1,7 @@
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../id_rsa.enc -out ../id_rsa -d
+chmod 600 ../deploy_key
